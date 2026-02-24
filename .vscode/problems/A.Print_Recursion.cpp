e818ca551b770db0db6e42dd67fc0ad652807232
nada <nadamoafia66@gmail.com>
2026-02-24 19:13:15 +0200

    Add solution for A. Print Recursion problem

---
+++ .vscode/problems/A.Print_Recursion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void print(int num){
+    if(num==0){
+        return;
+    }
+    cout<<"I love Recursion"<<endl;
+    print(num-1);
+}
+
+int  main(){
+    int num;
+    cin>>num;
+    print(num);
+    return 0;
+}
